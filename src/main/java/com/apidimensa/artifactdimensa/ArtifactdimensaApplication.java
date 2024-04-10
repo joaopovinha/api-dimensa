80371bdc6ab9cd69fa428a32ae7d4ebad642df95
Joao Povinha <joao.b.p@hotmail.com>
2024-04-10 01:57:38 -0300

    Uploading Project

---
+++ src/main/java/com/apidimensa/artifactdimensa/ArtifactdimensaApplication.java
@@ -0,0 +1,13 @@
+package com.apidimensa.artifactdimensa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtifactdimensaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtifactdimensaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apidimensa/artifactdimensa/ArtifactdimensaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apidimensa.artifactdimensa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtifactdimensaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
